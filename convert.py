f841d8d08e6db4e3bb5f3a50ac6702a0cf0b7726
Storm <sohila.samir1911@gmail.com>
2024-08-02 20:07:18 +0300

    new - supporting image formats and adding more video formats

--- convert.py
+++ convert.py
@@ -1,26 +1,56 @@
 import os
-import subprocess
 import sys
+import subprocess
+from PIL import Image
 
 
-source_folder = sys.argv[2]
-output_folder = sys.argv[4]
+source_folder = sys.argv[1]
+output_folder = sys.argv[2]
+from_format = sys.argv[3]
+to_format = sys.argv[4]
 
-source_path = os.path.join(os.path.dirname(__file__), source_folder)
-output_path = os.path.join(os.path.dirname(__file__), output_folder)
+videos_format = ["webm", "mp4", "mov", "gif"]
+images_format = ["png", "jpg", "jpeg", "webp"]
 
 def main():
+    source_path = os.path.join(os.path.dirname(__file__), source_folder)
+    output_path = os.path.join(os.path.dirname(__file__), output_folder)
+
+    if not (os.path.exists(source_path)):
+        print("Source path not found!")
+        return
+    
+    if (not from_format or not to_format):
+        print("Please provide source and target format!")
+        return
+
     if not os.path.exists(output_path):
         os.makedirs(output_path, exist_ok=True)
+    
+    # checking if the source and target format are supported video formats in the output folder
+    if from_format in videos_format and to_format in videos_format:
+        for video in os.listdir(source_path):
+            if video.endswith(from_format):
+                # check if the converted video already exists
+                if os.path.exists(f'{output_folder}/{video.split(".")[0]}.{to_format}'):
+                    print (f"{video.split('.')[0]}.{to_format} already exists!")
+                else:
+                    subprocess.run(f'ffmpeg -i {source_folder}/{video} -c vp9 -b:v 0 -crf 41 {output_folder}/{video.split(".")[0]}.{to_format}')
+    # checking if the source and target format are supported image formats
+    elif from_format in images_format and to_format in images_format:
+        for image in os.listdir(source_path):
+            if image.endswith(from_format):
+                # check if the converted image already exists in the output folder
+                if os.path.exists(f'{output_folder}/{image.split(".")[0]}.{to_format}'):
+                    print (f"{image.split('.')[0]}.{to_format} already exists!")
+                else:
+                    im = Image.open(f'{source_folder}/{image}')
+                    im.save(f'{output_folder}/{image.split(".")[0]}.{to_format}')
+    else:
+        print("Please provide valid/supported source and target format!")
+        return
 
-    for video in os.listdir(source_path):
-        if not (os.path.exists(f'{output_folder}/{video.split(".")[0]}.webm') or os.path.exists(f'{output_folder}/{video.split(".")[0]}.webp')):
-            if video.endswith(".gif"):
-                subprocess.run(f'ffmpeg -i {source_folder}/{video} -c vp9 -b:v 0 -crf 41 {output_folder}/{video.split(".")[0]}.webm')
-        else:
-            print(f'{video} already exists in the targeted directory!')
-
-    print("Done Converting!")
+    print("Done!")
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -2,10 +2,22 @@
 a handy python script that can be used to convert any .gif animated content to a .WebM format
 
 # Instructions
-the script is using the [ffmpeg](https://ffmpeg.org/) tool hence for it to work the tool must be installed on your machine
+the script is using [ffmpeg](https://ffmpeg.org/) tool for any videos conversion, hence for it to work the tool must be installed on your machine.
+
+# Supported Formats
+## Video Formats
+-   webm
+-   mp4
+-   mov
+-   gif
+## Image Formats
+-   png
+-   jpg
+-   jpeg
+-   webp
 
 # Get Started
 ## Clone the repository first
     git clone https://github.com/Sohila-Hashem/GIF-to-WebM-Convertor
 ## Run the code
-    python convert.py -s sourceDirectory -o outputDirectory
+    python convert.py sourceDirectory outputDirectory fromFormat toFormat
