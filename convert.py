0f9b175b682acc20289c60ab3ced791b4c82b534
Sohila-Hashem <sohila.samir1911@gmail.com>
2024-08-03 10:22:07 +0300

    bug_fix - fixing issues related to the moving and replacing functions

--- convert.py
+++ convert.py
@@ -15,24 +15,28 @@ def main():
     action_after_end = sys.argv[5]
 
     # utils
-    def replace_file(source_path, output_path, file_to_move):
-            shutil.replace(f'{source_path}/{file_to_move}.{from_format}', f'{output_path}/{file_to_move}.{to_format}')
+    def replace_file(from_path, to_path, file_to_move):
+            try:
+                os.rename(f'{from_path}/{file_to_move}.{from_format}', f'{from_path}/{file_to_move}.{to_format}')
+            except Exception as e:
+                print(e)
 
-    def move_file(output_path, file_to_move):
-            os.move(f'{output_path}/{file_to_move}.{to_format}')
+    def copy_file(from_path, to_path, file_to_move):
+            try:
+                shutil.copy(f'{to_path}/{file_to_move}.{to_format}', f'{from_path}/{file_to_move}.{to_format}')
+            except Exception as e:
+                print(e)
 
     supported_actions = {
         "replace": replace_file,
-        "move": move_file, 
+        "copy": copy_file, 
         "rp": replace_file, 
-        "mv": move_file
+        "cp": copy_file
     }
 
     is_action_provided = action_after_end in supported_actions
-    source_path = os.path.join(os.path.dirname(__file__), source_folder)
-    output_path = os.path.join(os.path.dirname(__file__), output_folder)
 
-    if not (os.path.exists(source_path)):
+    if not (os.path.exists(source_folder)):
         print("Source path not found!")
         return
     
@@ -40,32 +44,36 @@ def main():
         print("Please provide source and target format!")
         return
 
-    if not os.path.exists(output_path):
-        os.makedirs(output_path, exist_ok=True)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder, exist_ok=True)
 
     # checking if the source and target format are supported video formats in the output folder
     if from_format in videos_format and to_format in videos_format:
-        for video in os.listdir(source_path):
+        for video in os.listdir(source_folder):
             if video.endswith(from_format):
                 # check if the converted video already exists
                 if os.path.exists(f'{output_folder}/{video.split(".")[0]}.{to_format}'):
                     print (f"{video.split('.')[0]}.{to_format} already exists!")
+                    continue
                 else:
                     subprocess.run(f'ffmpeg -i {source_folder}/{video} -c vp9 -b:v 0 -crf 41 {output_folder}/{video.split(".")[0]}.{to_format}')
+
                     if (is_action_provided):
-                        supported_actions[action_after_end](output_path, video.split(".")[0], source_path=source_folder)
+                        supported_actions[action_after_end](source_folder, output_folder, video.split(".")[0])
     # checking if the source and target format are supported image formats
     elif from_format in images_format and to_format in images_format:
-        for image in os.listdir(source_path):
+        for image in os.listdir(source_folder):
             if image.endswith(from_format):
                 # check if the converted image already exists in the output folder
                 if os.path.exists(f'{output_folder}/{image.split(".")[0]}.{to_format}'):
                     print (f"{image.split('.')[0]}.{to_format} already exists!")
+                    continue
                 else:
                     im = Image.open(f'{source_folder}/{image}')
                     im.save(f'{output_folder}/{image.split(".")[0]}.{to_format}')
+
                     if (is_action_provided):
-                        supported_actions[action_after_end](output_path, image.split(".")[0], source_path=source_folder)
+                        supported_actions[action_after_end](source_folder, output_folder, image.split(".")[0])
     else:
         print("Please provide valid/supported source and target format!")
         return
